021dcf79e8933cd8b44464cc15b9081837400a66
artimidouae8-creator <artimidouae8@gmail.com>
2025-10-15 22:38:05 +0200

    Add files via upload

---
+++ nalubot.py
@@ -0,0 +1,8 @@
+print("Bonjour et bienvenue dans la prmière édition de Nalubot !")
+prénom = input("Comment tu t'appelles?")
+print("Ravi de te rencontrer" + prénom)
+progression = input("Es-tu prete à suivre ta progression?")
+if progression == "oui":
+    print(" J'ai déjà hate qu'on crée de grandes choses !")
+else:
+    print("Ne t'inquiète pas, tu vas aimer ça.")
